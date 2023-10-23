77fca5ba31dccb520659a970d068a88113dce3c8
MichaelDouglasLima <110948999+MichaelDouglasLima@users.noreply.github.com>
2023-10-23 12:27:00 -0300

    Add files via upload

---
+++ Atividade13/javascript.js
@@ -0,0 +1,12 @@
+
+function abrir(janela) {
+    janela.setAttribute("src", "assets/JanelaAberta.png");
+}
+
+function fechar(janela) {
+    janela.setAttribute("src", "assets/JanelaFechada.png");
+}
+
+function quebrar(janela) {
+    janela.setAttribute("src", "assets/JanelaQuebrada.png");
+}
