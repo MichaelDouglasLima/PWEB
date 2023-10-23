4ecb4843bc69e8008326c75ae122968daef82b1b
MichaelDouglasLima <110948999+MichaelDouglasLima@users.noreply.github.com>
2023-10-23 17:40:40 -0300

    Add files via upload

---
+++ Atividade14/javascript.js
@@ -0,0 +1,12 @@
+
+function toMaius() {
+    var novaFrase = document.getElementById("frase");
+
+    novaFrase.value = frase.value.toUpperCase();
+}
+
+function toMinus() {
+    var novaFrase = document.getElementById("frase");
+
+    novaFrase.value = frase.value.toLowerCase();
+}
